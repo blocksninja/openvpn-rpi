b1741d6dc36043592380a6b0e65baf113fd193aa
blocksninja <183516+blocksninja@users.noreply.github.com>
2019-07-23 00:13:07 +0200

    Updated image name.

--- publish.sh
+++ publish.sh
@@ -2,9 +2,9 @@
 set -e
 
 echo 'Pushing'
-docker push ryansch/openvpn:latest-amd64
-docker push ryansch/openvpn:latest-arm32v7
-docker push ryansch/openvpn:latest-arm64v8
+docker push appartment/openvpn-rpi:latest-amd64
+docker push appartment/openvpn-rpi:latest-arm32v7
+docker push appartment/openvpn-rpi:latest-arm64v8
 
 echo 'Pushing Manifest'
 manifest-tool push from-spec manifest.yml
